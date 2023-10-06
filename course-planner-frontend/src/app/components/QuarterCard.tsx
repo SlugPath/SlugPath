e9da7ac924febb4d0c319086bd7d083d0c3b37d4
oluthje <oluthje@gmail.com>
2023-10-06 15:07:34 -0700

    Init frontend

---
+++ course-planner-frontend/src/app/components/QuarterCard.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { Input, Button, Card } from '@mui/joy';
+
+export default function QuarterCard({ title }: { title: string }) {
+    const courses = ["CSE 12", "CSE 130", "PHIL 11"]
+
+    return (
+        <Card className="w-96">
+            {title}
+            {
+                courses.map((task, index) => (
+                    <Card
+                        className='flex-row justify-between'
+                        key={index}
+                        color='primary'
+                    >
+                        {task}
+                        <Button color='primary' variant='outlined'>
+                            X
+                        </Button>
+                    </Card>
+                ))
+            }
+        </Card>
+    )
+}
---
+++ course-planner-frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+import CoursePlanner from "./components/CoursePlanner"
+
+export default function Home() {
+  return (
+    <CoursePlanner/>
+  )
+}
