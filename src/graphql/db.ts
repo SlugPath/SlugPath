6a05ec0e733498ef543a3990e2d667a638914a9b
Furkan Ercevik <ercevik.furkan@gmail.com>
2023-10-07 11:29:16 -0700

    Updated db.ts to use prod db

--- src/graphql/db.ts
+++ src/graphql/db.ts
@@ -1,12 +1,20 @@
+import { Pool } from "pg";
 
-import {Pool} from 'pg';
+let pool;
+if (process.env.POSTGRES_URL !== undefined) {
+  pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+    max: 10,
+    idleTimeoutMillis: 30000,
+  });
+} else {
+  pool = new Pool({
+    host: "localhost",
+    port: 5432,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+}
 
-const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  database: process.env.POSTGRES_DB,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-});
-
-export {pool};
+export { pool };
