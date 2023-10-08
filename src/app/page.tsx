0a5bd735d96d30d69618ca3a5dfeceff10ee22e7
Furkan Ercevik <ercevik.furkan@gmail.com>
2023-10-08 11:15:41 -0700

    Used 'useState' hook in Index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,66 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Member } from "../graphql/member/schema";
 import CoursePlanner from "./components/CoursePlanner";
 
-async function getMembers(): Promise<Member[]> {
+async function fetchMembers(): Promise<Member[]>{
   // Retrieving information about team members
-  const query = 'query member {member { name, email }}';
+  const query = "query member {member { name, email }}";
 
   const prodHost = process.env.VERCEL_URL;
   const defaultUrl = "http://localhost:3000/api/graphql";
-
   const url = prodHost ? `https://${prodHost}/api/graphql` : defaultUrl;
 
-  console.log(`Sending query to ${url}`);
   const resMembers = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({query}),
+    body: JSON.stringify({ query }),
   });
+
   try {
     const jsonMembers = await resMembers.json();
-    return await jsonMembers.data.member;
+    return jsonMembers.data.member;
   } catch (e) {
     console.error(e);
     return [];
   }
-
-  // Returning a list of objects with member information
 }
 
-export default async function Home() {
-  const members = await getMembers();
+function Members() {
+  const [members, setMembers] = useState<Member[]>([]);
+  useEffect(() => {
+    fetchMembers().then((it: Member[]) => setMembers(it))
+  }, [])
+
+  if (members.length == 0) {
+    return (
+      <h2>Loading....</h2>
+    )
+  }
+  return (
+    <>
+      <div id="members" className="text-3xl pb-3">Meet the team members!</div>
+      {members.map((member) => (
+        <div
+          key={member.name}
+          className="grid grid-cols-2 place-items-center"
+        >
+          <div className="col-span-1">{member.name}</div>
+          <div className="col-span-1">{member.email}</div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default function Home() {
   return (
     <main>
       <div className="grid place-items-center py-10 gap-2">
-        <div className="text-3xl pb-3">Meet the team members!</div>
-        {members.map((member) => (
-          <div
-            key={member.name}
-            className="grid grid-cols-2 place-items-center"
-          >
-            <div className="col-span-1">{member.name}</div>
-            <div className="col-span-1">{member.email}</div>
-          </div>
-        ))}
+        <Members />
       </div>
       <CoursePlanner />
     </main>
