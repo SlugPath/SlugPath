9c4a4caeb9cda9dc613abdad555b41f5e037ad37
oluthje <oluthje@gmail.com>
2023-10-11 14:17:23 -0700

    Fixed apollo.ts issue

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,11 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { APP_URL } from '@/config'
 
-const prodHost = process.env.VERCEL_URL;
-const defaultUrl = "http://localhost:3000/api/graphql";
-const url = prodHost ? `https://${prodHost}/api/graphql` : defaultUrl;
 const apolloClient = new ApolloClient({
-//   uri: '/api/graphql',
-  uri: url,
+  uri: APP_URL + "/api/graphql",
   cache: new InMemoryCache(),
 })
 
