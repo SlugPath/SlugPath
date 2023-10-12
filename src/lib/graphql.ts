cd3646ab1b8d768ef7ebfe117272d2f8bf9e2967
Furkan Ercevik <ercevik.furkan@gmail.com>
2023-10-11 23:24:30 -0700

    Made a simple react hook to call GraphQL

---
+++ src/lib/graphql.ts
@@ -0,0 +1,38 @@
+import { APP_URL } from '@/config';
+import { useState, useEffect } from 'react';
+import { MemberQueryResult } from '@/app/ts-types/GraphQL';
+import { GraphQLQueryError } from '@/app/ts-types/Errors';
+
+// a graphQL query and any variables
+export function useMemberQuery(query: string) {
+  const [data, setData] = useState<MemberQueryResult | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<GraphQLQueryError | null>(null);
+
+  useEffect(() => {
+    // Define the GraphQL endpoint URL
+    const endpoint = `${APP_URL}/api/graphql`;
+
+    // Define the GraphQL request options
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    };
+
+    fetch(endpoint, options)
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, [query]);
+
+  return { data, loading, error };
+}
