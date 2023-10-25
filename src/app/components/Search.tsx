7992be2b89cf3e868296d7976fceb61dc384f78d
Teresa <tqwu@ucsc.edu>
2023-10-25 09:26:28 -0700

    Testing husky git hooks #2

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -1,6 +1,6 @@
 
-import { useState } from 'react';
-// import { gql, useQuery } from "@apollo/client";
+// import { useState } from 'react';
+import { gql, useQuery } from "@apollo/client";
 import { Button, Input, Option, Select } from '@mui/joy';
 
 export default function Search() {
