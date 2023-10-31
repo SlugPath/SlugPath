e1a9cdfbbd807b3064ff4bb1f2ff58831ac767dd
Teresa <tqwu@ucsc.edu>
2023-10-30 17:12:38 -0700

    #41 Disabled search button if not all inputs are provided

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -1,73 +1,100 @@
-
-import { useState } from "react";
+import React, { useState } from "react";
 import { gql, useQuery } from "@apollo/client";
-import { Button, Input, Option, Select } from '@mui/joy';
+import { Button, Input, Option, Select } from "@mui/joy";
 
 const GET_COURSE = gql`
   query getCourse($department: String!, $number: String!) {
-    coursesBy (
-      department: $department
-      number: $number
-    ) {
-      department, number, name
+    coursesBy(department: $department, number: $number) {
+      department
+      number
+      name
     }
   }
 `;
 
 export default function Search() {
+  const [department, setDepartment] = useState("");
+  const [number, setNumber] = useState("");
 
   const [search, setSearch] = useState(false);
-  const [courseDetails, setCourseDetails] = useState({
-    department: '',
-    number: ''
+  const [queryDetails, setQueryDetails] = useState({
+    department: "",
+    number: "",
   });
 
   const { data, loading, error } = useQuery(GET_COURSE, {
     variables: {
-      department: courseDetails.department,
-      number: courseDetails.number
+      department: queryDetails.department,
+      number: queryDetails.number,
     },
-    skip: !search // Skip the query if search button hasn't been pressed yet
+    skip: !search, // Skip the query if search button hasn't been pressed yet
   });
 
+  const handleChangeDepartment = (
+    event: React.SyntheticEvent | null,
+    newValue: string | null,
+  ) => {
+    setDepartment(newValue || "");
+  };
+
   const handleSearch = (departmentInput: string, numberInput: string) => {
-    setCourseDetails({department: departmentInput, number: numberInput.toUpperCase()});
+    setQueryDetails({
+      department: departmentInput,
+      number: numberInput.toUpperCase(),
+    });
     setSearch(true);
-  }
+  };
 
   return (
     <>
       {/* Search form begins */}
       <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = (Object.fromEntries((formData as any).entries()));
-            handleSearch(formJson.department, formJson.number);
-          }}
-        >
-        <div className="w-72 grid grid-cols-3">
-          <Select placeholder="Department" name="department" className="col-span-2">
-            <Option value="CSE">Computer Science & Engineering</Option>
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleSearch(department, number);
+        }}
+      >
+        <div className="grid grid-cols-3 gap-2 p-2">
+          <Select
+            placeholder="Department"
+            name="department"
+            aria-label="department"
+            className="col-span-2"
+            onChange={handleChangeDepartment}
+          >
+            <Option value="CSE" aria-label="computer science and engineering">
+              Computer Science & Engineering
+            </Option>
           </Select>
           <Input
+            className="col-span-1"
             color="neutral"
             placeholder="Number"
             size="md"
             variant="outlined"
             name="number"
-            className="col-span-1"
+            aria-label="number"
+            onChange={(event) => setNumber(event.target.value)}
           />
         </div>
-        <Button type="submit">Search</Button>
+        <div className="px-2 pb-2">
+          <Button
+            className="w-full"
+            aria-label="search"
+            disabled={department == "" || number == ""}
+            type="submit"
+          >
+            Search
+          </Button>
+        </div>
       </form>
       {/* Search form ends */}
 
       {/* Results begins */}
       {loading && <p>Loading...</p>}
-      {error && <p>Please enter a valid course number</p>}
+      {error && <p>No results found</p>}
       {data ? (
-        (data.coursesBy.length === 0) ? (
+        data.coursesBy.length === 0 ? (
           <div>
             <p>No results found</p>
           </div>
@@ -83,5 +110,5 @@ export default function Search() {
       )}
       {/* Results ends */}
     </>
-  )
+  );
 }
