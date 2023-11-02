c554dcb2ab76f7d4f6f75a8051632b27a99a0786
oluthje <oluthje@gmail.com>
2023-11-02 10:42:02 -0700

    Added useCoursePlanner hook

--- src/app/components/CoursePlanner.tsx
+++ src/app/components/CoursePlanner.tsx
@@ -1,179 +1,26 @@
-import { useState, useEffect } from "react";
-import { getCookie, setCookie } from "cookies-next";
 import QuarterCard from "./QuarterCard";
 import MajorCompletionModal from "./MajorCompletionModal";
 import ExportModal from "./ExportModal";
 import { initialPlanner } from "../../lib/initialPlanner";
 import { PlannerData } from "../ts-types/PlannerData";
-import { DragDropContext, DropResult, Droppable } from "@hello-pangea/dnd";
-import { isMobile, MobileWarningModal } from "./isMobile";
+import useCoursePlanner from "../hooks/useCoursePlanner";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Search from "./Search";
-import { createCourseFromId } from "../../lib/courseUtils";
-import { StoredCourse } from "../ts-types/Course";
+import { MobileWarningModal } from "./isMobile";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
 
 export default function CoursePlanner() {
-  const [courseState, setCourseState] = useState(initialPlanner);
-  const [showMajorCompletionModal, setShowMajorCompletionModal] =
-    useState(false);
-  const [showExportModal, setShowExportModal] = useState(false);
-  const [showMobileWarning, setShowMobileWarning] = useState(false);
-
-  // Runs upon initial render
-  useEffect(() => {
-    const cookieCourseState = getCookie("courseState");
-    if (cookieCourseState) {
-      console.log(JSON.parse(cookieCourseState) as PlannerData);
-      setCourseState(JSON.parse(cookieCourseState) as PlannerData);
-    }
-  }, []);
-
-  // Runs upon inital render: checks if user is on mobile device
-  useEffect(() => {
-    if (isMobile()) {
-      setShowMobileWarning(true);
-    }
-  }, []);
-
-  const handleCourseUpdate = (courseState: PlannerData) => {
-    setCourseState(courseState);
-
-    const json = JSON.stringify({
-      ...courseState,
-    });
-
-    setCookie("courseState", json);
-  };
-
-  const handleOnDragEnd = (result: DropResult) => {
-    const { destination, source, draggableId } = result;
-
-    if (!destination) return;
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    )
-      return;
-
-    // add course dragged from 'search-droppable' to quarter
-    if (source.droppableId === "search-droppable") {
-      const quarter = courseState.quarters[destination.droppableId];
-      const newStoredCourses = Array.from(quarter.courses);
-      newStoredCourses.splice(
-        destination.index,
-        0,
-        createCourseFromId(draggableId),
-      );
-      const newQuarter = {
-        ...quarter,
-        courses: newStoredCourses,
-      };
-
-      const newState = {
-        ...courseState,
-        quarters: {
-          ...courseState.quarters,
-          [newQuarter.id]: newQuarter,
-        },
-      };
-
-      handleCourseUpdate(newState);
-      return;
-    }
-
-    // delete course dragged into delete area or search-droppable
-    if (
-      destination.droppableId == "remove-course-area1" ||
-      destination.droppableId == "remove-course-area2" ||
-      destination.droppableId == "search-droppable"
-    ) {
-      const startQuarter = courseState.quarters[result.source.droppableId];
-      const newStoredCourses = Array.from(startQuarter.courses);
-      newStoredCourses.splice(result.source.index, 1);
-
-      const newQuarter = {
-        ...startQuarter,
-        courses: newStoredCourses,
-      };
-
-      const newState = {
-        ...courseState,
-        quarters: {
-          ...courseState.quarters,
-          [newQuarter.id]: newQuarter,
-        },
-      };
-
-      handleCourseUpdate(newState);
-      return;
-    }
-
-    const startQuarter = courseState.quarters[source.droppableId];
-    const finishQuarter = courseState.quarters[destination.droppableId];
-    if (startQuarter === finishQuarter) {
-      // moving course within startQuarter
-      const newStoredCourses = Array.from(startQuarter.courses);
-      newStoredCourses.splice(source.index, 1);
-      newStoredCourses.splice(
-        destination.index,
-        0,
-        startQuarter.courses[source.index],
-      );
-
-      const newQuarter = {
-        ...startQuarter,
-        courses: newStoredCourses,
-      };
-
-      const newState = {
-        ...courseState,
-        quarters: {
-          ...courseState.quarters,
-          [newQuarter.id]: newQuarter,
-        },
-      };
-
-      handleCourseUpdate(newState);
-    } else {
-      // moving course from startQuarter to finishQuarter
-      const movedStoredCourse = startQuarter.courses[source.index];
-      const startStoredCourses = Array.from(startQuarter.courses);
-      startStoredCourses.splice(source.index, 1);
-      const newStart = {
-        ...startQuarter,
-        courses: startStoredCourses,
-      };
-
-      const finishStoredCourses = Array.from(finishQuarter.courses);
-      finishStoredCourses.splice(destination.index, 0, movedStoredCourse);
-      const newFinish = {
-        ...finishQuarter,
-        courses: finishStoredCourses,
-      };
-
-      const newState = {
-        ...courseState,
-        quarters: {
-          ...courseState.quarters,
-          [newStart.id]: newStart,
-          [newFinish.id]: newFinish,
-        },
-      };
-
-      handleCourseUpdate(newState);
-    }
-  };
-
-  function getCoursesAlreadyAdded() {
-    const coursesAlreadyAdded: StoredCourse[] = [];
-    Object.values(courseState.quarters).forEach((quarter) => {
-      quarter.courses.forEach((course) => {
-        coursesAlreadyAdded.push(course);
-      });
-    });
-    return coursesAlreadyAdded;
-  }
+  const {
+    courseState,
+    handleDragEnd,
+    getCoursesAlreadyAdded,
+    showExportModal,
+    showMajorCompletionModal,
+    showMobileWarning,
+    setShowExportModal,
+    setShowMajorCompletionModal,
+  } = useCoursePlanner();
 
   return (
     <div className="bg-gray-100 mt-16">
@@ -191,7 +38,7 @@ export default function CoursePlanner() {
         showModal={showMajorCompletionModal}
       />
       <MobileWarningModal show={showMobileWarning} />
-      <DragDropContext onDragEnd={handleOnDragEnd}>
+      <DragDropContext onDragEnd={handleDragEnd}>
         <div className="flex">
           <div className="flex-1 px-4 py-6">
             <Search coursesAlreadyAdded={getCoursesAlreadyAdded()} />
---
+++ src/app/hooks/useCoursePlanner.ts
@@ -0,0 +1,182 @@
+import { createCourseFromId } from "../../lib/courseUtils";
+import { StoredCourse } from "../ts-types/Course";
+import { DropResult } from "@hello-pangea/dnd";
+import { isMobile } from "../components/isMobile";
+import { useState, useEffect } from "react";
+import { getCookie, setCookie } from "cookies-next";
+import { initialPlanner } from "../../lib/initialPlanner";
+import { PlannerData } from "../ts-types/PlannerData";
+
+export default function useCoursePlanner() {
+  const [courseState, setCourseState] = useState(initialPlanner);
+  const [showMajorCompletionModal, setShowMajorCompletionModal] =
+    useState(false);
+  const [showExportModal, setShowExportModal] = useState(false);
+  const [showMobileWarning, setShowMobileWarning] = useState(false);
+
+  // Runs upon initial render
+  useEffect(() => {
+    const cookieCourseState = getCookie("courseState");
+    if (cookieCourseState) {
+      console.log(JSON.parse(cookieCourseState) as PlannerData);
+      setCourseState(JSON.parse(cookieCourseState) as PlannerData);
+    }
+  }, []);
+
+  // Runs upon inital render: checks if user is on mobile device
+  useEffect(() => {
+    if (isMobile()) {
+      setShowMobileWarning(true);
+    }
+  }, []);
+
+  const handleCourseUpdate = (courseState: PlannerData) => {
+    setCourseState(courseState);
+
+    const json = JSON.stringify({
+      ...courseState,
+    });
+
+    setCookie("courseState", json);
+  };
+
+  const handleDragEnd = (result: DropResult) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    // add course dragged from 'search-droppable' to quarter
+    if (source.droppableId === "search-droppable") {
+      const quarter = courseState.quarters[destination.droppableId];
+      const newStoredCourses = Array.from(quarter.courses);
+      newStoredCourses.splice(
+        destination.index,
+        0,
+        createCourseFromId(draggableId),
+      );
+      const newQuarter = {
+        ...quarter,
+        courses: newStoredCourses,
+      };
+
+      const newState = {
+        ...courseState,
+        quarters: {
+          ...courseState.quarters,
+          [newQuarter.id]: newQuarter,
+        },
+      };
+
+      handleCourseUpdate(newState);
+      return;
+    }
+
+    // delete course dragged into delete area or search-droppable
+    if (
+      destination.droppableId == "remove-course-area1" ||
+      destination.droppableId == "remove-course-area2" ||
+      destination.droppableId == "search-droppable"
+    ) {
+      const startQuarter = courseState.quarters[result.source.droppableId];
+      const newStoredCourses = Array.from(startQuarter.courses);
+      newStoredCourses.splice(result.source.index, 1);
+
+      const newQuarter = {
+        ...startQuarter,
+        courses: newStoredCourses,
+      };
+
+      const newState = {
+        ...courseState,
+        quarters: {
+          ...courseState.quarters,
+          [newQuarter.id]: newQuarter,
+        },
+      };
+
+      handleCourseUpdate(newState);
+      return;
+    }
+
+    const startQuarter = courseState.quarters[source.droppableId];
+    const finishQuarter = courseState.quarters[destination.droppableId];
+    if (startQuarter === finishQuarter) {
+      // moving course within startQuarter
+      const newStoredCourses = Array.from(startQuarter.courses);
+      newStoredCourses.splice(source.index, 1);
+      newStoredCourses.splice(
+        destination.index,
+        0,
+        startQuarter.courses[source.index],
+      );
+
+      const newQuarter = {
+        ...startQuarter,
+        courses: newStoredCourses,
+      };
+
+      const newState = {
+        ...courseState,
+        quarters: {
+          ...courseState.quarters,
+          [newQuarter.id]: newQuarter,
+        },
+      };
+
+      handleCourseUpdate(newState);
+    } else {
+      // moving course from startQuarter to finishQuarter
+      const movedStoredCourse = startQuarter.courses[source.index];
+      const startStoredCourses = Array.from(startQuarter.courses);
+      startStoredCourses.splice(source.index, 1);
+      const newStart = {
+        ...startQuarter,
+        courses: startStoredCourses,
+      };
+
+      const finishStoredCourses = Array.from(finishQuarter.courses);
+      finishStoredCourses.splice(destination.index, 0, movedStoredCourse);
+      const newFinish = {
+        ...finishQuarter,
+        courses: finishStoredCourses,
+      };
+
+      const newState = {
+        ...courseState,
+        quarters: {
+          ...courseState.quarters,
+          [newStart.id]: newStart,
+          [newFinish.id]: newFinish,
+        },
+      };
+
+      handleCourseUpdate(newState);
+    }
+  };
+
+  function getCoursesAlreadyAdded() {
+    const coursesAlreadyAdded: StoredCourse[] = [];
+    Object.values(courseState.quarters).forEach((quarter) => {
+      quarter.courses.forEach((course) => {
+        coursesAlreadyAdded.push(course);
+      });
+    });
+    return coursesAlreadyAdded;
+  }
+
+  return {
+    courseState,
+    handleDragEnd,
+    getCoursesAlreadyAdded,
+    showMajorCompletionModal,
+    setShowMajorCompletionModal,
+    showExportModal,
+    setShowExportModal,
+    showMobileWarning,
+  };
+}
