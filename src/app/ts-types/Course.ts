489041f485550230ec51aa18024358e1d5f8ae70
Teresa <tqwu@ucsc.edu>
2023-11-05 15:08:22 -0800

    Fixing deployment

--- src/app/ts-types/Course.ts
+++ src/app/ts-types/Course.ts
@@ -1,3 +1,11 @@
+export interface Course {
+  department: string;
+  number: string;
+  name: string;
+  id: string;
+  credits: number;
+}
+
 /**
  * `StoredCourse` is a type to represent how courses are stored.
  * `number` is stored as a string, as some course numbers are appended with letters.
