500acd01416e74ea6cdc367ab0f6c5e6e660b743
Furkan Ercevik <ercevik.furkan@gmail.com>
2023-11-13 14:16:03 -0800

    Added colors to GradProgress

--- src/app/components/GradProgress.tsx
+++ src/app/components/GradProgress.tsx
@@ -1,8 +1,16 @@
-import { LinearProgress, Box, Typography } from "@mui/joy";
+import { LinearProgress, Box, Typography, ColorPaletteProp } from "@mui/joy";
 
 const TOTAL_CREDITS_NEEDED = 180;
+const colors = ["danger", "warning", "primary", "success"];
+
+const getColor = (percentage: number) => {
+  const idx = Math.floor((colors.length - 1) * percentage);
+  console.log(`${colors[idx]}`);
+  return colors[idx] ?? "neutral";
+};
 
 export const GradProgress = ({ credits }: { credits: number }) => {
+  const percentage = Math.min(credits / TOTAL_CREDITS_NEEDED, 1);
   return (
     <>
       <Typography level="h4" fontSize="lg" sx={{ mb: 0.5 }}>
@@ -13,8 +21,8 @@ export const GradProgress = ({ credits }: { credits: number }) => {
           thickness={20}
           determinate
           variant="outlined"
-          color="success"
-          value={Math.min((credits / TOTAL_CREDITS_NEEDED) * 100, 100)}
+          color={getColor(percentage) as ColorPaletteProp}
+          value={percentage * 100}
         />
       </Box>
     </>
