54c7641c9c66a11eed88025a2d41092af4ace0c5
Teresa <tqwu@ucsc.edu>
2023-11-14 01:25:41 -0800

    #51 Added disclaimer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
-    <footer className="bg-white h-10 grid place-items-center">
-      <p className="text-gray-600 text-center p-2 text-sm">
+    <footer className="flex bg-white h-10 grid place-items-center">
+      <p className="text-gray-600 text-center text-xs self-end pt-2">
+        Please check with your Academic and Major advisor(s) for the most
+        accurate information.
+      </p>
+      <p className="text-gray-600 text-center text-xs pb-2">
         © 2023 UCSC Course Planner. All rights reserved.
       </p>
     </footer>
