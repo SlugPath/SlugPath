6ad53726117f5c8dbeb8193a5706f7284ffa6c65
oluthje <oluthje@gmail.com>
2023-11-15 09:36:59 -0800

    Improved CourseCard delete button

--- src/app/components/CourseCard.tsx
+++ src/app/components/CourseCard.tsx
@@ -1,8 +1,16 @@
-import { Card, CardContent, Grid, Link, Typography } from "@mui/joy";
+import {
+  Card,
+  CardContent,
+  Grid,
+  IconButton,
+  Link,
+  Typography,
+} from "@mui/joy";
 import { StoredCourse } from "../types/Course";
 import { getTitle } from "../../lib/courseUtils";
 import { useState } from "react";
 import { DraggableProvided } from "@hello-pangea/dnd";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function CourseCard({
   course,
@@ -21,7 +29,6 @@ export default function CourseCard({
   isDragging: boolean;
   onShowCourseInfoModal: any;
 }) {
-  const [showDelete, setShowDelete] = useState(false);
   const [highlighted, setHighlighted] = useState(false);
   const margin = 2;
   const getItemStyle = (draggableStyle: any) => ({
@@ -71,27 +78,16 @@ export default function CourseCard({
           </Grid>
           <Grid xs={2}>
             {onDelete !== undefined && (
-              <button
-                onMouseEnter={() => setShowDelete(true)}
-                onMouseLeave={() => setShowDelete(false)}
+              <IconButton
                 onClick={() => onDelete(index)}
-                style={{ opacity: showDelete ? 1 : 0.2 }}
+                variant="plain"
+                size="sm"
+                className={`bg-gray-200 hover:bg-gray-300 ${
+                  highlighted ? "" : "invisible"
+                }`}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="black"
-                  className="w-4 h-4"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
+                <CloseIcon fontSize="small" />
+              </IconButton>
             )}
           </Grid>
         </Grid>
