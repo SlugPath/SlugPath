3c577ad1712d946daf34517393b4fb23b0ef6cb7
Teresa <tqwu@ucsc.edu>
2023-11-16 17:17:46 -0800

    #108 Added warning icon for zero planner page

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -88,9 +88,25 @@ export default function App() {
                 To get started, click the <b>+</b> button above to create a new
                 planner.
               </div>
-              <div className="text-sm text-slate-600 text-center pt-4">
-                (Login to save your progress - Your progress will not be saved
-                if you login after creating a planner)
+              <div className="text-sm text-slate-600 flex flex-row items-end gap-2 text-center pt-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="red"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                  />
+                </svg>
+                <span>
+                  (Login to save your progress - Your progress will not be saved
+                  if you login after creating a planner)
+                </span>
               </div>
             </div>
           ) : (
