a5ef37d7465eeac42ffe129f95a9d94036b18223
Furkan Ercevik <ercevik.furkan@gmail.com>
2024-02-05 15:15:52 -0800

    Adjusted ExportSkeleton and fixed env var issue with migrate.yml

--- src/app/components/modals/exportModal/ExportSkeleton.tsx
+++ src/app/components/modals/exportModal/ExportSkeleton.tsx
@@ -9,18 +9,19 @@ const cardData = Array.from({ length: 16 }, (_, index) => ({
 
 export default function ExportSkeleton() {
   return (
-    <div className="mx-24">
+    <div className="mx-auto size-3/4 mb-5">
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <Card variant="outlined" className="flex flex-row">
+          <Card variant="outlined" className="flex flex-row items-end">
             <Skeleton variant="text" />
             <Skeleton variant="circular" width={48} height={48} />
           </Card>
         </Grid>
         {cardData.map((card) => (
           <Grid item key={card.id} xs={12} sm={6} md={4} lg={3}>
-            <Card variant="outlined" sx={{ width: "100%", height: "100%" }}>
-              <Skeleton variant="rectangular" sx={{ height: 80 }} />
+            <Card variant="outlined" sx={{ width: "100%", height: "80%" }}>
+              <Skeleton variant="text" />
+              <Skeleton variant="rectangular" sx={{ height: 40 }} />
             </Card>
           </Grid>
         ))}
