c9cfcd11554a93c6f9fe16e3699c8ef2ca2cc343
Nihal Ernest <nernest2002@gmail.com>
2024-02-29 14:06:23 -0800

    latest share code

--- src/app/planner/[share]/page.tsx
+++ src/app/planner/[share]/page.tsx
@@ -7,12 +7,16 @@ import { clonePlanner } from "@/lib/plannerUtils";
 
 import { authOptions } from "@/lib/auth";
 
+import { v4 as uuidv4 } from "uuid";
+
+import { redirect } from "next/navigation";
+
 
 
 
 export default async function Page({ params }: { params: { share: string } }) {
   console.log("In Share")
-  console.log("Slug = " + params.share)
+  console.log("share id = " + params.share)
 
   const session = await getServerSession(authOptions);
 
@@ -26,12 +30,28 @@ export default async function Page({ params }: { params: { share: string } }) {
   let sharedPlanner = await getPlannerById(params.share)
 
   // clone the planner
-  let duplicatePlanner = await clonePlanner(sharedPlanner)
+  let duplicatePlanner = clonePlanner(sharedPlanner)
 
-  planners.unshift(duplicatePlanner)
 
-  console.log(`user: ${session?.user.id}`)
-  console.log(`planners: ${planners}`)
+  // give the clone a new ID
+  console.log(`Old planner id: ${duplicatePlanner.id}`)
+
+
+  let newId = uuidv4()
+  duplicatePlanner.id = newId
+
+  console.log(`New planner id: ${duplicatePlanner.id}`)
+
+
+  // console.log(`planners before: ${planners}`)
+
+  console.log(`ORIGINAL PLANNER\n\n${JSON.stringify(sharedPlanner, null, 2)}}`)
+  console.log(`DUPLICATED PLANNER\n\n${JSON.stringify(duplicatePlanner, null, 2)}}`)
+
+  planners.push(duplicatePlanner)
+
+  console.log(`user id: ${session?.user.id}`)
+  // console.log(`planners: ${JSON.stringify(planners, null, 2)}`)
 
   
   // save it
@@ -49,5 +69,7 @@ export default async function Page({ params }: { params: { share: string } }) {
   
 
   console.log("/share test")
-  return <Planners planners={planners}/>;
+
+  redirect("/planner");
+
 }
