3265c99b1db232c953153a930e7ee64ea5acaaaf
aajoseph-dev <aajoseph@ucsc.edu>
2024-03-01 03:38:46 -0800

    added more features to toolbar

--- src/app/components/notesEditor/NotesEditor.tsx
+++ src/app/components/notesEditor/NotesEditor.tsx
@@ -1,10 +1,25 @@
-import Link from "@tiptap/extension-link";
+import BulletList from "@tiptap/extension-bullet-list";
+import { Color } from "@tiptap/extension-color";
+import ListItem from "@tiptap/extension-list-item";
+import OrderedList from "@tiptap/extension-ordered-list";
+import Subscript from "@tiptap/extension-subscript";
+import Superscript from "@tiptap/extension-superscript";
+import TextStyle from "@tiptap/extension-text-style";
 import Underline from "@tiptap/extension-underline";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { useState } from "react";
 
-const extensions = [StarterKit, Underline, Link];
+const extensions = [
+  StarterKit,
+  Underline,
+  Superscript,
+  Subscript,
+  ListItem,
+  BulletList,
+  OrderedList,
+  TextStyle,
+  Color,
+];
 
 export interface NotesEditorProps {
   content: string;
@@ -16,7 +31,6 @@ export default function NotesEditor({
   content,
   onUpdateNotes,
 }: NotesEditorProps) {
-  const [linkUrl, setLinkUrl] = useState("");
   const editor = useEditor(
     {
       extensions,
@@ -41,22 +55,17 @@ export default function NotesEditor({
     formattingFunction();
   };
 
-  const toggleLink = () => {
-    if (editor.isActive("link")) {
-      editor.chain().focus().unsetLink().run();
-    } else {
-      editor.chain().focus().setLink({ href: linkUrl }).run();
-    }
-  };
-
   const buttonBaseClasses =
-    "px-2 py-1 text-sm font-medium rounded transition-colors duration-150";
+    "px-2 py-1 text-sm font-medium rounded transition-colors duration-150 mr-1";
+  const colorButtonClass =
+    "text-sm font-medium rounded transition-colors duration-150 mr-1";
   const activeClasses = "bg-blue-500 text-white hover:bg-blue-600";
   const inactiveClasses = "bg-gray-200 text-gray-800 hover:bg-gray-300";
   const disabledClasses = "opacity-50 cursor-not-allowed";
 
   return (
     <div className="border-1 border-red-50 h-full">
+      {/* Bold Button */}
       <button
         onMouseDown={(event) =>
           applyFormatting(event, () =>
@@ -70,6 +79,7 @@ export default function NotesEditor({
       >
         Bold
       </button>
+      {/* Italic Button */}
       <button
         onMouseDown={(event) =>
           applyFormatting(event, () =>
@@ -83,6 +93,7 @@ export default function NotesEditor({
       >
         Italic
       </button>
+      {/* Underline Button */}
       <button
         onMouseDown={(event) =>
           applyFormatting(event, () =>
@@ -96,6 +107,7 @@ export default function NotesEditor({
       >
         Underline
       </button>
+      {/* Subscript Button */}
       <button
         onMouseDown={(event) =>
           applyFormatting(event, () =>
@@ -109,22 +121,71 @@ export default function NotesEditor({
       >
         Strike
       </button>
-      {/* Input field for link URL */}
+      {/* Color Picker */}
       <input
-        type="text"
-        placeholder="Enter URL"
-        value={linkUrl}
-        onChange={(e) => setLinkUrl(e.target.value)}
-        className="px-2 py-1 text-sm border rounded"
+        type="color"
+        onMouseDown={(event) => event.preventDefault()}
+        onChange={(event) =>
+          editor.chain().focus().setColor(event.target.value).run()
+        }
+        value={editor.getAttributes("textStyle").color || "#000000"}
+        className={`${colorButtonClass} cursor-pointer`}
       />
-      {/* Button to apply link */}
+      {/* Superscript Button */}
       <button
-        onMouseDown={(event) => applyFormatting(event, toggleLink)}
-        disabled={!linkUrl}
-        className="px-2 py-1 text-sm font-medium rounded transition-colors duration-150 bg-blue-500 text-white hover:bg-blue-600"
+        onMouseDown={(event) =>
+          applyFormatting(event, () =>
+            editor.chain().focus().toggleSuperscript().run(),
+          )
+        }
+        disabled={!editor.can().toggleSuperscript()}
+        className={`${buttonBaseClasses} ${
+          editor.isActive("superscript") ? activeClasses : inactiveClasses
+        }`}
       >
-        Toggle Link
+        Superscript
       </button>
+      {/* Subscript Button */}
+      <button
+        onMouseDown={(event) =>
+          applyFormatting(event, () =>
+            editor.chain().focus().toggleSubscript().run(),
+          )
+        }
+        disabled={!editor.can().toggleSubscript()}
+        className={`${buttonBaseClasses} ${
+          editor.isActive("subscript") ? activeClasses : inactiveClasses
+        }`}
+      >
+        Subscript
+      </button>
+      {/* Unordered List Button */}
+      <button
+        onMouseDown={(event) =>
+          applyFormatting(event, () =>
+            editor.chain().focus().toggleBulletList().run(),
+          )
+        }
+        className={`${buttonBaseClasses} ${
+          editor.isActive("bulletList") ? activeClasses : inactiveClasses
+        }`}
+      >
+        Bullet List
+      </button>
+      {/* Ordered List Button */}
+      <button
+        onMouseDown={(event) =>
+          applyFormatting(event, () =>
+            editor.chain().focus().toggleOrderedList().run(),
+          )
+        }
+        className={`${buttonBaseClasses} ${
+          editor.isActive("orderedList") ? activeClasses : inactiveClasses
+        }`}
+      >
+        Numbered List
+      </button>
+      {/* Undo Button */}
       <button
         onMouseDown={(event) =>
           applyFormatting(event, () => editor.chain().focus().undo().run())
@@ -136,6 +197,7 @@ export default function NotesEditor({
       >
         Undo
       </button>
+      {/* Redo Button */}
       <button
         onMouseDown={(event) =>
           applyFormatting(event, () => editor.chain().focus().redo().run())
