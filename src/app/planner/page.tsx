99bbebbbf6061807856181b7e15a767eab0ad17d
Furkan Ercevik <ercevik.furkan@gmail.com>
2024-03-14 11:18:21 -0700

    / redir all logged in users, /planner doesn't redir unauth users

--- src/app/planner/page.tsx
+++ src/app/planner/page.tsx
@@ -5,19 +5,17 @@ import {
   dehydrate,
 } from "@tanstack/react-query";
 import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 
 import { getAllPlanners } from "../actions/planner";
 
 export default async function Home() {
   const session = await getServerSession();
   const queryClient = new QueryClient();
-  if (!session) redirect("/");
 
   await queryClient.prefetchQuery({
     queryKey: ["planners"],
     queryFn: async () => {
-      const userEmail = session.user.email ?? "";
+      const userEmail = session?.user.email ?? "";
       if (userEmail.length === 0) return [];
       return await getAllPlanners(userEmail);
     },
