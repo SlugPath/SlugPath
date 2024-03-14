7c30168931912bf19f22f06f09f3dc262784db9b
cecilenoguera <cecilenoguera55@gmail.com>
2024-03-14 16:58:58 -0700

    UI updates to quarters display

--- src/app/components/modals/courseInfoModal/QuartersOffered.tsx
+++ src/app/components/modals/courseInfoModal/QuartersOffered.tsx
@@ -18,9 +18,6 @@ export default function QuartersOffered({
     instructor: string;
   }>;
 }) {
-  const years = ["20-21", "21-22", "22-23", "23-24"];
-  const quarters = ["Fall", "Winter", "Spring", "Summer"];
-
   if (!enrollmentInfo) {
     return null;
   }
@@ -35,92 +32,120 @@ export default function QuartersOffered({
     return Array.from(professorsSet);
   };
 
+  const getUniqueYears = () => {
+    if (!enrollmentInfo) return [];
+    const years = new Set<string>();
+    enrollmentInfo.forEach(({ term }) => {
+      years.add(term.catalogYear);
+    });
+    return Array.from(years);
+  };
+
+  const years = getUniqueYears();
+  const quarters = ["Fall", "Winter", "Spring", "Summer"];
+
   return (
-    <div style={{ marginBottom: "-0.5rem" }}>
-      <Typography component="p">Quarters Offered:</Typography>
-      <Tabs
-        aria-label="Basic tabs"
-        defaultValue={0}
-        size="sm"
-        sx={{
-          "--Tab-indicatorThickness": "3px",
-          "--Tabs-spacing": "15px",
-          bgcolor: "background.level1",
-          borderRadius: "sm",
-        }}
-      >
-        <TabList
-          underlinePlacement="bottom"
+    <div>
+      <Typography component="p" sx={{ marginBottom: "0.1rem" }}>
+        Quarters Offered:
+      </Typography>
+      {enrollmentInfo && enrollmentInfo.length > 0 && (
+        <Tabs
+          aria-label="Basic tabs"
+          defaultValue={0}
+          size="sm"
           sx={{
-            borderRadius: "0px",
-            /*             bgcolor: 'background.level1',
-            [`& .${tabClasses.root}[aria-selected="true"]`]: {
-              boxShadow: 'sm',
-              bgcolor: 'background.surface',
-            }, */
+            "--Tab-indicatorThickness": "3px",
+            "--Tabs-spacing": "15px",
+            bgcolor: "background.level1",
+            borderRadius: "sm",
+            "& .MuiTab-root:first-child": {
+              borderTopLeftRadius: "6px",
+            },
           }}
         >
+          <TabList
+            underlinePlacement="bottom"
+            sx={{
+              borderRadius: "0px",
+            }}
+          >
+            {years.map((year, index) => (
+              <Tab key={index}> 20{year}</Tab>
+            ))}
+          </TabList>
           {years.map((year, index) => (
-            <Tab key={index}> 20{year}</Tab>
-          ))}
-        </TabList>
-        {years.map((year, index) => (
-          <TabPanel key={index} value={index}>
-            <Sheet
-              sx={{
-                maxHeight: 300,
-                overflow: "auto",
-              }}
-            >
-              <Table
-                aria-label="table with sticky header"
-                stickyHeader
-                borderAxis="both"
-                sx={(theme) => ({
-                  '& th[scope="col"]': theme.variants.outlined.neutral,
-                })}
+            <TabPanel key={index} value={index} sx={{ margin: "-0.25rem" }}>
+              <Sheet
+                sx={{
+                  maxHeight: 300,
+                  overflow: "auto",
+                  borderRadius: "sm",
+                }}
               >
-                <thead>
-                  <tr>
-                    <th scope="col" style={{ width: "10%" }}>
-                      Quarter
-                    </th>
-                    <th scope="col">Professors</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {quarters.map((quarter, index) => (
-                    <tr key={index}>
-                      <td>{quarter}</td>
-                      <td>
-                        <div className="flex justify-start flex-wrap items-center gap-2">
-                          {getProfessorsForYearAndQuarter(year, quarter).map(
-                            (professor, index) => (
-                              <Chip
-                                key={index}
-                                color={getQuarterColor(
-                                  quarter as
-                                    | "Fall"
-                                    | "Winter"
-                                    | "Spring"
-                                    | "Summer",
-                                )}
-                              >
-                                {" "}
-                                {professor}{" "}
-                              </Chip>
-                            ),
-                          )}
-                        </div>
-                      </td>
+                <Table
+                  aria-label="table with sticky header"
+                  stickyHeader
+                  borderAxis="both"
+                  sx={(theme) => ({
+                    '& th[scope="col"]': theme.variants.outlined.neutral,
+                    borderRadius: "sm",
+                    "& tbody > tr:last-child > td:first-child": {
+                      borderBottomLeftRadius: "6px",
+                    },
+                    "& tbody > tr:last-child > td:last-child": {
+                      borderBottomRightRadius: "6px",
+                    },
+                  })}
+                >
+                  <thead>
+                    <tr>
+                      <th
+                        scope="col"
+                        style={{
+                          width: "10%",
+                          textAlign: "center",
+                          borderRadius: "sm",
+                        }}
+                      >
+                        Quarter
+                      </th>
+                      <th scope="col">Professors</th>
                     </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </Sheet>
-          </TabPanel>
-        ))}
-      </Tabs>
+                  </thead>
+                  <tbody>
+                    {quarters.map((quarter, index) => (
+                      <tr key={index}>
+                        <td style={{ textAlign: "center" }}>{quarter}</td>
+                        <td>
+                          <div className="flex justify-start flex-wrap items-center gap-2">
+                            {getProfessorsForYearAndQuarter(year, quarter).map(
+                              (professor, index) => (
+                                <Chip
+                                  key={index}
+                                  color={getQuarterColor(
+                                    quarter as
+                                      | "Fall"
+                                      | "Winter"
+                                      | "Spring"
+                                      | "Summer",
+                                  )}
+                                >
+                                  {professor}
+                                </Chip>
+                              ),
+                            )}
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </Sheet>
+            </TabPanel>
+          ))}
+        </Tabs>
+      )}
     </div>
   );
 }
